290fd5d31c2ef71a238a5ba366104ed3bff336bb
Abhinav Raj <abhinavraj1904@gmail.com>
2026-07-27 20:09:07 +0530

    Add query-based document retrieval in main.py

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from loaders.pdf_loaders import load_pdf
 from rag.chunking import create_chunks
 from models.embedding import get_embedding
 from models.vector_store import create_vector_store
+from rag.retriever import get_retriever
+
 
 def main():
     documents = load_pdf()
     chunks = create_chunks(documents)
     embedding_model = get_embedding()
     vector_store = create_vector_store(chunks , embedding_model)
+    query = input("Enter yor Query:")
+    retriever = get_retriever(vector_store)
+    result = retriever.invoke(query)
 
     print(f"Total Documents: {len(documents)}")
     print(f"Total Chunks: {len(chunks)}")
     print("Vector Store Created Successfully")
+    print(f"Query: {query} \n NexusGPT: {result[0].page_content}")
 
 if __name__ == "__main__":
     main()
