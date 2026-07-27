cbc048d7203183de06949f5846e2317828d6c843
Abhinav Raj <abhinavraj1904@gmail.com>
2026-07-28 02:52:00 +0530

    implement FAISS local storage

--- models/vector_store.py
+++ models/vector_store.py
@@ -6,3 +6,14 @@ def create_vector_store(chunks , embeddings_model):
         embedding= embeddings_model
     )
     return vector_store
+
+def save_vector_local(vector_store ,db_path):
+    vector_store.save_local(db_path)
+
+def load_vector_local(db_path , embedding):
+    vector_database = FAISS.load_local(
+        db_path,
+        embedding,
+        allow_dangerous_deserialization=True
+    )
+    return vector_database
